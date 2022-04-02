1e4f425ced39c73cacd70a55965483ba3fe1163b
asdasz <jasio.waszut@gmail.com>
2022-04-02 17:32:21 +0200

    Improving Deck

--- Deck.java
+++ Deck.java
@@ -1,23 +1,74 @@
-public class Deck {
-    private Card cards[];
+package com.company;
+import javax.swing.*;
+import java.util.Random;
+import java.util.ArrayList;
+public class Deck
+{
+    private Card[] cards;
+    private int cardsindeck;
 
-    public Deck() {
+    public Deck()
+    {
         cards = new Card[24];
     }
 
-    public void reset() {
+    public void reset()
+    {
         Card.Color[] colors = Card.Color.values();
-        Card.Value[] values = Card.Value.values();
-        for(int q=0; q<6; q++) {
-            Card.Value value = values[q];
-            for(int w=0; w<4; w++) {
-                Card.Color color = colors[w];
-                cards[6*q + w] = new Card(color, value);
+        cardsindeck=0;
+
+        for(int i=0; i<4; i++)
+        {
+            Card.Color color = colors[i];
+            for(int j=0; j<6; j++)
+            {
+                cards[cardsindeck++] = new Card(color, Card.Value.getValue(j));
             }
+        }
+
+    }
+
+    public void replaceDeckWith(ArrayList<Card> cards){
+        this.cards=cards.toArray(new Card[cards.size()]);
+        this.cardsindeck = this.cards.length;
+    }
+
+    public boolean isEmpty() {
+        return cardsindeck== 0;
+    }
+
+    public void shuffle()
+    {
+        int n = cards.length;
+        Random random = new Random();
+        for(int i=0; i<cards.length; i++)
+        {
+            int randomvalue = i + random.nextInt(n-1);
+            Card randomCard = cards[randomvalue] = cards[i];
+            cards[i] = randomCard;
         }
     }
 
-    public Card drawCard(int index) {
-        return cards[index];
+    public Card drawCard() throws IllegalArgumentException{
+        if(isEmpty()){
+            throw new IllegalArgumentException("No more cards");
+        }
+        return cards[--cardsindeck];
+    }
+    public ImageIcon drawCardImage() throws IllegalArgumentException{
+        if(isEmpty()){
+            throw new IllegalArgumentException("No more cards");
+
+        }
+        return new ImageIcon((cards[--cardsindeck].toString()+".png"));
+    }
+
+    public Card[] drawCards(int n){
+        Card[] ret = new Card[n];
+
+        for(int i=0; i<n; i++){
+            ret[i] = cards[--cardsindeck];
+        }
+        return ret;
     }
 }
